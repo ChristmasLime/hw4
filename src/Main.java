79c63edb3568e6e5307b8a10ae3a0a27d3e71e6f
ChristmasLime <christmasboom@mail.ru>
2023-02-06 15:01:33 +0300

    Задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,33 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " , то он совершеннолетний.");
+        }else{
+            System.out.println("Если возраст человека равен " + age + " , то он не достиг совершеннолетия, нужно немного подождать.");
+        }
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-
+        int temperature = 2;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
+        }else{
+            System.out.println("На улице " + temperature + " градусов,  можно идти без шапки.");
+        }
     }
 
 
     public static void task3 () {
         System.out.println("Задача 3");
+        int speed = 150;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " , то придется заплатить штраф");
+        }else {
+            System.out.println("Если скорость " + speed + " , то можно ездить спокойно.");
+        }
 
     }
 
